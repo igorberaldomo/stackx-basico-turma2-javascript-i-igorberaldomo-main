65404de5a735989f6c6921e41ce41f1b5e072d2f
Igor Beraldo <igorberaldomo@gmail.com>
2024-04-01 10:36:30 -0300

    tarefa 1 javascript

---
+++ exercicios.js
@@ -0,0 +1,136 @@
+// -------------------------------------
+
+/**
+ * Função que calcula a soma de 2 números
+ * @param {number} a - O primeiro número.
+ * @param {number} b - O segundo número.
+ * @returns {number} A soma dos dois numeros
+ */
+function soma(a, b) {
+  var soma = a + b;
+  return soma;
+}
+
+/**
+ * Função que calcula a multiplicação de 2 números
+ * @param {number} a - O primeiro número.
+ * @param {number} b - O segundo número.
+ * @returns {number} A multiplicação dos dois numeros
+ */
+function multiplicacao(a, b) {
+  var multiplicacao = a * b;
+  return multiplicacao;
+}
+
+/**
+ * Função que calcula a divisão de 2 números
+ * @param {number} a - O primeiro número.
+ * @param {number} b - O segundo número.
+ * @returns {number} A divisão dos dois numeros
+ */
+function divisao(a, b) {
+  var divisao =  a/b;
+  return divisao;
+}
+
+/**
+ * Função que calcula a subtração de 2 números
+ * @param {number} a - O primeiro número.
+ * @param {number} b - O segundo número.
+ * @returns {number} A subtração dos dois numeros
+ */
+function subtracao(a, b) {
+  var subtracao =  a -b;
+  return subtracao;
+}
+
+/**
+ * Função que resto da divisão de 2 números
+ * @param {number} a - O primeiro número.
+ * @param {number} b - O segundo número.
+ * @returns {number} O resto da divisão dos dois numeros
+ */
+function modulo(a, b) {
+  var modulo = a %b;
+  return modulo;
+}
+
+/**
+ * Função que calcula o maior número entre 2
+ * @param {number} a - O primeiro número.
+ * @param {number} b - O segundo número.
+ * @returns {number} O maior número
+ */
+function max(a, b) {
+  if(a > b){
+    return a;
+  }else{
+    return b;
+  }
+}
+
+// -------------------------------------
+
+/**
+ * Função que concatena 2 strings utilizando template strings
+ * @param {string} a - A primeira string
+ * @param {string} b - A segunda string
+ * @returns {string} As 2 strings concatenadas
+ */
+function strConcat(a, b) {
+  let frase1 = a+' '+b;
+  return frase1;
+}
+
+// --------------------------------------
+
+/**
+ * Função que implementa controle de fluxo
+ * 
+ * @param {number} numero - O número em questão
+ * @returns {string} 
+ *  - "Maior que 10" quando o número é maior que 10
+ *  - "Igual a 10" quando o número é igual a 10
+ *  - "Menor que 10" quando o número é menor que 10
+ */
+function retornaNumero(numero) {
+  let frase;
+  if( numero > 10){
+    frase = "Maior que 10";
+  }else if(numero == 10){
+    frase = "Igual a 10";
+  }else{
+    frase = "Menor que 10";
+  }
+  return frase;
+}
+
+// --------------------------------------
+
+/**
+ * Função que valida se o número em questão é positivo
+ * @param {number} num - Número em questão
+ * @returns {boolean} Retorna true para números positivos
+ */
+function ehPositivo(num) {
+  let resultado
+  if(num <= 0){
+    resultado = false;
+  }else{
+    resultado = true;
+  }
+  return resultado;
+}
+
+module.exports = 
+{ 
+  soma,
+  multiplicacao,
+  divisao,
+  subtracao,
+  modulo,
+  max,
+  strConcat,
+  retornaNumero,
+  ehPositivo
+};
